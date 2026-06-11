0fbb17b22883d11a0d4f16271ff49f296d6f11ed
Russell-Sedj <russelldossouyovo@gmail.com>
2026-06-11 17:16:00 +0200

    mon travail

---
+++ src/evaluate.py
@@ -0,0 +1,18 @@
+"""Squelette d'évaluation pour AvisSense."""
+
+
+def evaluate() -> None:
+    """Prépare la future évaluation du modèle."""
+    # TODO: Calculer accuracy.
+    # TODO: Calculer precision.
+    # TODO: Calculer recall.
+    # TODO: Calculer f1-score.
+    print("Évaluation non encore implémentée.")
+
+
+def main() -> None:
+    evaluate()
+
+
+if __name__ == "__main__":
+    main()
